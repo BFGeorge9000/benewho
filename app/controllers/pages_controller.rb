802a91a2f8c08e6c44c461e1da89593728f0a02d
BFGeorge9000 <georgemilberger@hotmail.com>
2014-11-09 21:08:46 -0500

    Improve name generation for greater variety

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
   def show
-    yaml = YAML.load_file("#{Rails.root}/config/name.yml")
+    yaml = normalize_splits(YAML.load_file("#{Rails.root}/config/name.yml"))
     @name = generate_name(yaml)
   end
+
+  private
+
+  def normalize_splits(data_set)
+    new_data_set = data_set.dup
+    data_set.keys.map do |key|
+      (data_set[key]["splits"]["front"].count + data_set[key]["splits"]["back"].count).times do
+        new_data_set[key]["completes"] << "split"      
+      end
+    end
+    new_data_set
+  end
+
 end
